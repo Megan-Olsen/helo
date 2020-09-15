90e03560df53931065667328734ca6facef870b3
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-15 13:45:04 -0600

    um ducks?

--- server/controller.js
+++ server/controller.js
@@ -3,12 +3,14 @@ const bcrypt = require('bcryptjs')
 module.exports = {
     register: async (req, res) => {
         const db = req.app.get('db')
-        const { username, password } = req.body
+        const {username, password} = req.body
 
         const [user] = await db.check_user([username])
-        if (user) { 
-            return res.status(409).send('User Already Exists')
+
+        if(user){ 
+            return res.status(409).send('user exists')
         }
+        
         const salt = bcrypt.getSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
 
@@ -19,22 +21,22 @@ module.exports = {
         res.status(200).send(req.session.user)
     },  
     login: (req, res) => {
-
+        const db = req.app.get('db')
     },
     logout: (req, res)=> {
-
+        const db = req.app.get('db')
     },  
     getUser: (req, res)=> {
-
+        const db = req.app.get('db')
     },  
     getPostsUser: (req, res)=> {
-
+        const db = req.app.get('db')
     },  
     getPostId: (req, res)=> {
-
+        const db = req.app.get('db')
     },  
     postNew: (req, res)=> {
-
+        const db = req.app.get('db')
     },  
 
 }
