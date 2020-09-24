6811a5355f2a2ad6e4e7218ecae96ea293db40fe
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-23 22:36:36 -0600

    umm

--- server/index.js
+++ server/index.js
@@ -11,12 +11,14 @@ const app = express()
 const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
 
 app.use(express.json())
-app.use(session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 1000* 60 * 60 * 24 * 365}
-}))
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {maxAge: 1000* 60 * 60 * 24 * 365}
+    })
+)
 
 //Endpoints go here
 app.post('/api/auth/register', ctrl.register)
